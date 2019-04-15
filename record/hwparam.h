69ef164de10f5e5e4b7d733663d044b7f1c2fd5a
niallmarshall <48561556+niallmarshall@users.noreply.github.com>
2019-04-15 21:19:38 +0100

    Add files via upload

---
+++ record/hwparam.h
@@ -0,0 +1,44 @@
+/**
+* @file hwparam.h
+* @brief defines hardware parameters
+* @author songwongtp, Niall Marshall
+* @date 07/04/2019
+
+*/
+
+#ifndef WAV_H
+#define WAV_H
+
+/** @brief Use the newer ALSA API */
+#define ALSA_PCM_NEW_HW_PARAMS_API //!<Updated ALSA hw API
+#include <alsa/asoundlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string>
+#include <utility>
+
+/** @brief Class to define static variables, for samplePCM */
+class waveRecorder {
+
+public:
+ 
+    static uint32_t file_size;
+    static char format_marker[4];
+    static uint16_t format_type;
+    static uint16_t number_of_channels;
+    static uint32_t sample_rate;
+    static uint32_t bytes_per_second;
+    static uint16_t bytes_per_frame;
+    static uint16_t bits_per_sample;
+
+    char * recordWAV(); 
+    char *buffer;
+
+    //constructor
+    waveRecorder();
+    ~waveRecorder(){ delete[] buffer; }
+};
+
+#endif
